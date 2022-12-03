b1412e1ed0d17e2192978ab4d5f53b87077c60a1
RashedRifat88 <rasrif88@gmail.com>
2022-12-03 20:00:49 +0600

    image correction....home pathology.........

--- resources/views/admin/home_pathology/show_patient_order.blade.php
+++ resources/views/admin/home_pathology/show_patient_order.blade.php
@@ -3,6 +3,115 @@
 
 <head>
     @include('admin.css')
+    <style>
+        img {
+            border-radius: 5px;
+            cursor: pointer;
+            transition: 0.3s;
+        }
+
+        img:hover {
+            opacity: 0.7;
+        }
+
+        /* The Modal (background) */
+        .modal {
+            display: none;
+            /* Hidden by default */
+            position: fixed;
+            /* Stay in place */
+            z-index: 1;
+            /* Sit on top */
+            padding-top: 100px;
+            /* Location of the box */
+            left: 0;
+            top: 0;
+            width: 100%;
+            /* Full width */
+            height: 100%;
+            /* Full height */
+            overflow: auto;
+            /* Enable scroll if needed */
+            background-color: rgb(0, 0, 0);
+            /* Fallback color */
+            background-color: rgba(0, 0, 0, 0.9);
+            /* Black w/ opacity */
+        }
+
+        /* Modal Content (image) */
+        .modal-content {
+            margin: auto;
+            display: block;
+            width: 80%;
+            max-width: 700px;
+        }
+
+        /* Caption of Modal Image */
+        #caption {
+            margin: auto;
+            display: block;
+            width: 80%;
+            max-width: 700px;
+            text-align: center;
+            color: #ccc;
+            padding: 10px 0;
+            height: 150px;
+        }
+
+        /* Add Animation */
+        .modal-content,
+        #caption {
+            -webkit-animation-name: zoom;
+            -webkit-animation-duration: 0.6s;
+            animation-name: zoom;
+            animation-duration: 0.6s;
+        }
+
+        @-webkit-keyframes zoom {
+            from {
+                -webkit-transform: scale(0)
+            }
+
+            to {
+                -webkit-transform: scale(1)
+            }
+        }
+
+        @keyframes zoom {
+            from {
+                transform: scale(0)
+            }
+
+            to {
+                transform: scale(1)
+            }
+        }
+
+        /* The Close Button */
+        .close {
+            position: absolute;
+            top: 15px;
+            right: 35px;
+            color: #f1f1f1;
+            font-size: 40px;
+            font-weight: bold;
+            transition: 0.3s;
+        }
+
+        .close:hover,
+        .close:focus {
+            color: #bbb;
+            text-decoration: none;
+            cursor: pointer;
+        }
+
+        /* 100% Image Width on Smaller Screens */
+        @media only screen and (max-width: 700px) {
+            .modal-content {
+                width: 100%;
+            }
+        }
+    </style>
 </head>
 
 <body>
@@ -107,11 +216,20 @@
                                             @foreach ($data as $obj1)
                                                 <tr>
 
-
-                                                    <td><img height="100 px" width="100 px"  src="test_prescription_image/{{ $obj1->test_prescription }}"></td>
+                                                    <td><img id='image_{{ $obj1->id }}' height="100 px"
+                                                            width="100 px"
+                                                            src="test_prescription_image/{{ $obj1->test_prescription }}">
+                                                    </td>
                                                     <td style="">{{ $obj1->pat_name }}</td>
                                                     <td style="">{{ $obj1->pat_mobile }}</td>
-                                                    {{-- <td style="">{{ $obj1->test_list }}</td> --}}
+                                                    @php
+                                                        $var = collect($obj1->test_list)->implode("\n");
+                    
+                                                    @endphp
+                                                    <td style="">
+                                                        <pre>{{  $var  }}</pre>
+                                                    </td>
+                                                    {{-- <td style="">{{ implode(', ', $obj1->test_list) }}</td> --}}
                                                     <td style="">{{ $obj1->date }}</td>
 
                                                     {{-- <td style="">{{ $obj1->message }}</td> --}}
@@ -157,7 +275,36 @@
     <!-- container-scroller -->
 
     @include('admin.script')
+    {{-- modal for image --}}
+    <div id="myModal" class="modal">
+        <span class="close" style="cursor:pointer;margin-top:70px; margin-right:120px;">&times;</span>
+        <img class="modal-content" id="img01">
+        <div id="caption"></div>
+    </div>
 
+    <script>
+        // Get the modal
+        $(document).ready(function() {
+            $('img').click(function() {
+                var image_id = $(this).attr('id');
+                var modal = document.getElementById("myModal");
+                // Get the image and insert it inside the modal - use its "alt" text as a caption
+                var img = document.getElementById(image_id);
+                var modalImg = document.getElementById("img01");
+                var captionText = document.getElementById("caption");
+                modal.style.display = "block";
+                modalImg.src = this.src;
+                captionText.innerHTML = this.alt;
+            });
+            // Get the <span> element that closes the modal
+            var span = document.getElementById("myModal");
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+                console.log('ok');
+                span.style.display = "none";
+            }
+        });
+    </script>
 </body>
 
 </html>
